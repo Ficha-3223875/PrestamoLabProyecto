7f0ddafb6bcf734ef11cafb23090d866ac4a3e17
Andres-Betancur-Castañeda <ac20061234@gmail.com>
2026-09-23 21:52:50 -0500

    Test completos y funcionales

--- app/src/test/java/com/example/prstamolabctma/ApiServiceTest.kt
+++ app/src/test/java/com/example/prstamolabctma/ApiServiceTest.kt
@@ -12,6 +12,7 @@ import org.junit.Test
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+
 class ApiServiceTest {
 
     private lateinit var mockWebServer: MockWebServer
